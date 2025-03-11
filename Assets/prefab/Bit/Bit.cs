cfaedc5bebe65e5e6d6118600843b93f0c5cb015
39052@ma-web.nl <166252789+JosHub911@users.noreply.github.com>
2025-03-11 13:57:14 +0100

    changed
    s

--- Assets/prefab/Bit/Bit.cs
+++ Assets/prefab/Bit/Bit.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class Bit : MonoBehaviour
 {
-    // Start is called before the first frame update
+    SpriteRenderer spriteRenderer;
+    public bool state = false;
     void Start()
     {
-        
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        if (state)
+        {
+            spriteRenderer.color = Color.green;
+        }
+        else
+        {
+            spriteRenderer.color = Color.red;
+        }
+    }
+    private void OnMouseUp()
+    {
+        state = !state;
     }
 }
